d726c4b38ffee5371bdff656d16b6fe2f95f03bc
Kafayaaa <rizqikafamuntaqa777@gmail.com>
2025-12-23 23:44:53 +0700

    chore: boilerplate project

---
+++ src/api/products/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { createClient } from "../../../lib/supabase/server";
+
+export async function GET() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("products").select("*");
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json(data);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const supabase = await createClient();
+
+  const { error } = await supabase.from("products").insert(body);
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ success: true });
+}
