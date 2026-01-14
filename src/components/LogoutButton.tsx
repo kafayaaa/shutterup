07a4f631dabe47bb143c1181ddfaa395f5f18ee2
Kafayaaa <rizqikafamuntaqa777@gmail.com>
2026-01-14 15:19:31 +0700

    feat: add logout button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,9 +1,11 @@
 export default function LogoutButton() {
   return (
     <form action="/logout" method="post">
-      <button type="submit" className="text-zinc-950 dark:text-zinc-50">
-        Logout
-      </button>
+      <div className="w-full flex flex-col items-center justify-center">
+        <button type="submit" className="text-zinc-950 dark:text-zinc-50">
+          Logout Account
+        </button>
+      </div>
     </form>
   );
 }
