e2ead772c9467076c271d9970d50d96167c6d4e1
Kafayaaa <rizqikafamuntaqa777@gmail.com>
2026-01-14 15:27:11 +0700

    fix: address PR review feedback

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,9 @@
 export default function LogoutButton() {
   return (
     <form action="/logout" method="post">
-      <div className="w-full flex flex-col items-center justify-center">
-        <button type="submit" className="text-zinc-950 dark:text-zinc-50">
-          Logout Account
-        </button>
-      </div>
+      <button type="submit" className="text-zinc-950 dark:text-zinc-50">
+        Logout Account
+      </button>
     </form>
   );
 }
